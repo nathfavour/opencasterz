17aed6e055412118ede596e4fb40a34b75ed4053
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-01-17 01:10:25 +0100

    ⏰  Add layout dimensions, timing, and app information constants in utils

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Constants {
+  // App information
   static const String appName = 'OpenCasterz';
   static const String apiEndpoint = 'https://api.opencasterz.com';
 
-  // Additional Constants
+  // Layout dimensions
+  static const double headerHeight = 80.0;
   static const double borderRadius = 12.0;
+
+  // Timing
   static const Duration animationDuration = Duration(milliseconds: 300);
+
+  // Colors
   static const Color primaryColor = Colors.blueAccent;
   static const Color accentColor = Colors.redAccent;
 
